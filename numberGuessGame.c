03d88222e94c51c2ffced0592c461715a0010f43
Raushan Rayn <83304272+RaynRaushan@users.noreply.github.com>
2022-02-18 12:17:03 +0530

    Add files via upload

---
+++ numberGuessGame.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+int genratenum(int n, int e)
+{
+    srand(time(NULL));
+    return (rand() % (e - n + 1)) + n;
+}
+int main()
+{
+    int a, b, count = 0;
+    b = genratenum(1, 50);
+    printf("      ***GAME***\n");
+    printf("###choose a number same as a computer for win###\n\n");
+    printf("choose a number between 1 to 50.\n");
+    do
+    {
+
+        scanf("%d", &a);
+        if (a == b)
+        {
+            printf("Mission complete\n");
+        }
+        else if (b > a)
+        {
+            printf("Some upper please\n");
+        }
+        else if (b < a)
+        {
+            printf("Some lower please\n");
+        }
+        count++;
+    } while (a != b);
+    printf("----you got success in %d times-----", count);
+    return 0;
+}
